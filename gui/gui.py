e28c12594000eb1257ba7a3950105be7516eca02
Khoa <khoa.h0086881@gmail.com>
2025-03-27 03:32:34 +0700

    GUI is integrated with backend

--- gui/gui.py
+++ gui/gui.py
@@ -1,7 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from tkinter import * 
 from tkinter import ttk
 import tkinter as tk
 import pandas as pd
+from route_finding.world import World 
 
 def model_selection():
     selected_model = combo_box.get()
@@ -19,27 +24,11 @@ def compute_date():
     date = date_entry.get()
     return date
 
-def get_data(site_data):
-    origin_id = compute_origin()
-    destination_id = compute_destination()
-    date = compute_date()
-    model = model_selection()
-
-    if not origin_id:
-        origin_id = 970
-    if not destination_id:
-        destination_id = 3001
-
-
-    print(site_data)
-    print("--------------------")
-    print(f"Origin: {origin_id}\nDestination: {destination_id}\nDate: {date}\nModel: {model}")
-
 # Read data file
 site_data = pd.read_csv("../data/Scats Data.csv", encoding='utf-8', sep='\t').fillna(0)
 
 app = tk.Tk()
-app.geometry("600x400")
+app.geometry("600x600")
 
 app.grid_columnconfigure(0, weight=1)
 app.resizable(False, False)
@@ -52,8 +41,8 @@ combo_box_label = tk.Label(app, text="Select your preferred model: *", font=("Ar
 date_label = tk.Label(app, text="Date/Time: *", font=("Arial", 13)).grid(row=6, pady=5)
 
 # Create a Combobox widget
-combo_box = ttk.Combobox(app, width=30, values=["Option 1", "Option 2", "Option 3"])
-combo_box.set("Option 1")
+combo_box = ttk.Combobox(app, width=30, values=["LSTM", "GRU", "SAES"], state="readonly")
+combo_box.set("LSTM")
 combo_box.grid(row=5, pady=5)
 
 origin_entry = Entry(app, width=30,)
@@ -65,6 +54,53 @@ destination_entry.grid(row=3, pady=5)
 date_entry = Entry(app, width=30,)
 date_entry.grid(row=7, pady=5)
 
-ttk.Button(app, width=15, text="Find routes", command=get_data(site_data)).grid(row=8, pady=10)
+# Create a Text widget to display multi-line text, but set it to readonly (non-editable)
+text_box = tk.Text(app, height=15, width=60)
+text_box.config(state=tk.DISABLED)  # Set to read-only (non-editable)
+text_box.grid(row=9, pady=20)
+
+def find_route(site_data):
+    origin_id = compute_origin()
+    destination_id = compute_destination()
+    date = compute_date()
+    model = model_selection()
+
+    if not origin_id:
+        origin_id = 970
+    if not destination_id:
+        destination_id = 3001
+
+    world = World(site_data, origin_id, destination_id)
+    paths = world.search_no_param()
+
+    return paths
+
+def button_listener():
+    result = find_route(site_data)
+    text_data = ""
+
+    if result:
+        i = 1
+        for path in result:
+            print(f"----- Route {i} -----")
+            text_data += f"----- Route {i} -----\n"
+            print(path)
+            text_data += str(path)
+            text_data += "\n\n"
+            print("\n")
+            i += 1
+    else:
+        error_msg = "Error: Fail to compute path.\n"
+        print(error_msg)
+        print("-------------------------------")
+        text_data += error_msg
+
+    text_box.config(state=tk.NORMAL)  # Make the Text widget editable temporarily
+    text_box.delete(1.0, tk.END)
+    text_box.insert(tk.END, text_data)
+
+ttk.Button(app, width=15, text="Find routes", command=button_listener).grid(row=8, pady=10)
+
+
 
 app.mainloop()
